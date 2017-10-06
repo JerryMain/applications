786d387ac59fafe5ad79da3c6428a30b49d1d5a1
JerryMain <public.lorann@outlook.com>
2017-10-06 15:16:00 +0200

    Initial setup for microcontroller cursus application

---
+++ uC_cursus/README.md
@@ -0,0 +1,2 @@
+This directory contains the software for the custom nucleo shield
+which was used during the microcontroller cursus.
---
+++ uC_cursus/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("RIOT is currently running on a(n) %s board.\n", RIOT_BOARD);
+
+    return 0;
+}
